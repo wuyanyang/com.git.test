ac06e371a1b15f6a0d624f69bca6eac5fa51be05
bob <nuanrenxinshi@icloud.com>
2022-04-12 17:59:16 +0800

    second commit

--- src/main/java/com/xbyang/git/GitTest.java
+++ src/main/java/com/xbyang/git/GitTest.java
@@ -3,5 +3,7 @@ package com.xbyang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("second commit");
     }
 }
