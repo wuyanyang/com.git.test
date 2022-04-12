38a8c0e4c91c2babdd4c6ecfd2d7cc47666838d3
bob <nuanrenxinshi@icloud.com>
2022-04-12 18:10:53 +0800

    hot-fix commit

--- src/main/java/com/xbyang/git/GitTest.java
+++ src/main/java/com/xbyang/git/GitTest.java
@@ -9,5 +9,9 @@ public class GitTest {
         System.out.println("hello git 3");
 
         System.out.println("hot fix git4");
+
+        System.out.println("冲突");
+
+        System.out.println("hot-fix 冲突");
     }
 }
